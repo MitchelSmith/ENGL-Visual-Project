738008941dc1177c247bc13a5583ce34483b08ed
Mitchel Smith <mitchel.smith@yahoo.com>
2018-03-22 16:10:10 -0500

    Added basic structure.

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express"),
+    app     = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+app.get("/index", function(req, res) {
+    res.render("index");
+});
+
+app.get("*", function(req, res) {
+    res.render("index");
+});
+
+app.listen(3000, function() {
+    console.log("Listening at port 3000...");
+});
