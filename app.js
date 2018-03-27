c0887cdc18bf636b61e179e39fe123b582dd2681
Mitchel Smith <mitchel.smith@yahoo.com>
2018-03-27 12:29:24 -0500

    Added more content and structure.

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ app.get("/home", function(req, res) {
     res.render("home");
 });
 
+app.get("/resident-spotlight", function(req, res) {
+    res.render("resident-spotlight")
+})
+
+app.get("/solstice-festival", function(req, res) {
+    res.render("solstice-festival")
+})
+
+app.get("/news/local-resident-joins-circus", function(req, res) {
+    res.render("news/local-resident-joins-circus");
+})
+
+app.get("/news/preparing-for-this-years-solstice", function(req, res) {
+    res.render("news/preparing-for-this-years-solstice");
+})
+
+app.get("/news/the-man-who-wanted-to-fly", function(req, res) {
+    res.render("news/the-man-who-wanted-to-fly");
+})
+
 app.get("*", function(req, res) {
     res.render("home");
 });
